a0d6933a3535e44f96f724813671f3f5677e0dac
김정진 <rww4287@gmail.com>
2017-01-23 13:29:28 +0900

    수업내용 packageTest

---
+++ PakageTest/src/com/ktds/edu/me/Main.java
@@ -0,0 +1,19 @@
+package com.ktds.edu.me;
+
+import com.ktds.edu.me.hospital.Doctor;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Package Test 입니다..");
+		
+		Doctor doctor = new Doctor();
+		
+		doctor.hello();
+		System.out.println();
+	}
+	
+
+
+}
---
+++ PakageTest/src/com/ktds/edu/me/hospital/Doctor.java
@@ -0,0 +1,11 @@
+package com.ktds.edu.me.hospital;
+
+public class Doctor {
+	
+	public void hello() {
+
+
+		System.out.println("안녕하세요 의사입니다");
+		
+	}
+}
