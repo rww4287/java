77b22200bbbd646bbc90346aff6f74fc63b4e043
김정진 <rww4287@gmail.com>
2017-01-23 09:48:52 +0900

    숙제2

--- Test/src/Homework2.java
+++ Test/src/Homework2.java
@@ -22,15 +22,15 @@ public class Homework2 {
 				int score5=input.nextInt();
 				int sum=score1+score2+score3+score4+score5;
 				double avg = sum/5.0;
-				String message = String.format("총점은 %d 평균은 %.2f점 입니다",sum,avg);
-				System.out.println(message);
+				String message1 = String.format("총점은 %d 평균은 %.2f점 입니다",sum,avg);
+				System.out.println(message1);
 			}
 			else if(select==2){
 				System.out.println("섭씨 온도를 입력하세요");
 				int C = input.nextInt();
 				double result = (C*1.8)+32;
-				String message = String.format("화씨 %.2fºF 입니다",result);
-				System.out.println(message);
+				String message2 = String.format("화씨 %.2fºF 입니다",result);
+				System.out.println(message2);
 			}
 			else if(select==3){
 				System.out.println("몸무게를 입력하세요(kg)");
@@ -38,8 +38,8 @@ public class Homework2 {
 				System.out.println("키를 입력하세요(m)");
 				double cm = input.nextDouble();
 				double result = (double) kg / (cm*cm);
-				String message = String.format("BMI는 %.2f 입니다",result);
-				System.out.println(message);
+				String message3 = String.format("BMI는 %.2f 입니다",result);
+				System.out.println(message3);
 			}
 			else if(select==4) {
 				break;
